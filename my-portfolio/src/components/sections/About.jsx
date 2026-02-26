7840ef3387a57dd706441a091b97ba2283462320
jaycsucc27 <jay.genita@csucc.edu.ph>
2026-02-26 22:57:08 +0800

    enhance About section with detailed education and internship experience; update Home section with personal introduction

--- my-portfolio/src/components/sections/About.jsx
+++ my-portfolio/src/components/sections/About.jsx
@@ -57,18 +57,21 @@ export const About = () =>{
                 hover:shadow-cyan-400/50
                 transition-all duration-300 ease-in-out">
                     
-    <h3 className="text-center text-xl font-old mb-4">
+    <h3 className="text-center text-xl font-bold mb-4">
        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="blue" className="size-8 inline-block mr-5">
   <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
 </svg>
  Education
 
     </h3>
-    
-    
-    <h3>Bachelors Degree in Computer Engineering</h3>
-
-
+<ul className="list-disc list-inside text-gray-300 space-y-2">
+    <li>
+        <strong>Bachelor's Degree in Computer Engineering</strong> - Caraga State University - Cabadbaran Campus (2021-2025)
+    </li>
+     <li>
+            <strong>Relevant Coursework:</strong> Data Structures and Algorithms, Web Development, Software Engineering, Database Systems, Computer Networks
+        </li>
+</ul>
 </div>
 <div >
       <div className="p-6 rounded-xl border border-white/10 
@@ -85,7 +88,16 @@ export const About = () =>{
         Work Experience
 
     </h3>
-    <h3>Bachelors Degree in Computer Engineering</h3>
+   
+    <ul className=" text-gray-300 space-y-2">
+    <li>
+        <strong>Frontend Developer Intern </strong> at Jairosoft Inc. (June 2024 - August 2024)
+       
+    </li>
+     <p>
+            assisted in developing and maintaining the company's main web application using React and TailwindCSS, collaborated with the design team to implement responsive UI components, and participated in code reviews to ensure code quality and best practices.
+        </p>
+</ul>
     </div>
     
     
--- my-portfolio/src/components/sections/Home.jsx
+++ my-portfolio/src/components/sections/Home.jsx
@@ -5,17 +5,17 @@ export const Home = ()=>{
 
         <div className="text-center z-10 px-4">
             <h1 className="text-5xl md:text-7xl font-bold mb-6 
-                            bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text 
+                            bg-linear-to-r from-blue-500 to-cyan-400 bg-clip-text 
                             text-transparent leading-tight">
-                Hi!
+                Hi! Im Jay Genita
             </h1>
             <p className="text-gray-400 text-lg mb-8 max-w-large mx-auto">
-                fadaadadadadadv
+               
             </p>
             <div className="flex justify-center space-x-4">
 <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded 
                                 font-medium transition relative overflow-hidden 
-                                hover:-translate-y-1 hover:shadow-[0_0_15px_rgba(59,_10,_246,_0.4)]">
+                                hover:-translate-y-1 hover:shadow-[0_0_15px_rgba(59,10,246,0.4)]">
     View Projects
 </a>
 <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
